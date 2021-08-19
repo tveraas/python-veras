04879ff55bb13b689484d56485a330de2c0c7d14
tveraas <thiagovrq@gmail.com>
2021-08-19 23:22:20 +0000

    com banco de dados

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
+
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = 'Veraldo'
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/hello')
+def pagina_inicial():
+    return "Hello World"
